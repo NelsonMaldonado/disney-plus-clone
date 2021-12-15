ab7661779af73f186522e0cbc512d26f483ea4ed
Nelson Maldonado <play2nino@gmail.com>
2021-12-15 00:17:45 -0800

    slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImageSlider from './ImageSlider'
 
 
 export default function Home() {
     return (
         <Container>
+            <ImageSlider/>
            Home Component 
         </Container>
     )
@@ -14,4 +16,17 @@ const Container = styled.main `
 min-height: calc(100vh - 70px);
 
 padding: 0 calc(3.5vw + 5px);
+position: relative;
+&: before {
+    background-image: url("/images/home-background.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+}
 ` 
