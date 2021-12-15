cef0ade660567f4f1946a14f42ce4809ef8e8dd2
Nelson Maldonado <play2nino@gmail.com>
2021-12-14 20:01:04 -0800

    styled nav

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -2,15 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 function Headers() {
     return (
-        <Container>
-            Header
-        </Container>
+        <Nav>
+            <Logo/>
+           
+        </Nav>
     )
 }
 
 export default Headers
 
-const Container = styled.div`
+const Nav = styled.nav`
 height: 70px;
 background-color: #090b13;
+`
+const Logo = styled.img `
+width: 80px;
+height: 
 `
