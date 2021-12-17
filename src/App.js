429e926bf15e2c0ff5be5568ad0fe9fd7a622414
Nelson Maldonado <play2nino@gmail.com>
2021-12-16 18:07:21 -0800

    installed react router dom 5 2 0 and it works

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import Headers from "./components/Headers"
 // import { Counter } from './features/counter/Counter';
 import "./App.css"
 import Home from "./components/Home"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Detail from "./components/Detail"
 
 function App() {
   return (
     <div>
-      <Headers />
+      <Router>
+        <Headers />
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
+
       <Home />
     </div>
   )
